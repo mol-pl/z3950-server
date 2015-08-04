6f3dc23ce2502c1e48fed7e8a55df6adf3814ce1
pawelmol <pawel@mol.com.pl>
2015-08-04 10:49:09 +0000

    refaktor - serwer z39.50 uniezależniony od projektu molnet
    git-svn-id: https://svn.mol.com.pl/svn/mol.net/src/trunk/java/z3950-server@8455 74f6632f-f9af-439b-9eda-9f9433cb0ea0

--- src/test/java/TestITCase.java
+++ src/test/java/TestITCase.java
@@ -24,38 +24,5 @@ public class TestITCase {
         isbns[3] = "83-01-01373-7 coś";
         isbns[4] = "coś 83-01-01373-7 coś";
         isbns[5] = "coś 83-01-01373-1 coś  83-01-01373-2";
-
-        String s = getNormalizedISBN(isbns);
-    }
-
-    private String getNormalizedISBN(String[] isbnFieldList) {
-        Pattern pattern = Pattern.compile("[^0-9X]+");
-        if (isbnFieldList != null) {
-            for (String isbnField : isbnFieldList) {
-                if (isbnField != null) {
-                    String[] splitedIsbn = isbnField.split(" ");
-                    //dla każdego wyłuskanego po spacji isbn
-                    for (String s : splitedIsbn) {
-                        //usuwanie z ciągu znków - zostają tylko cyfry i X
-                        String normalizedIsbn = pattern.matcher(s).replaceAll("");
-
-                        if (normalizedIsbn.startsWith("978")) {
-                            //zwracaj 13 znaków
-                            if (normalizedIsbn.length() >= 13) {
-                                return normalizedIsbn.substring(0, 13);
-                            }
-                        } else if (normalizedIsbn.length() >= 10) {
-                            //zwracja 10 znaków
-                            return normalizedIsbn.substring(0, 10);
-                        }
-                    }
-                } else {
-                    return null;
-                }
-            }
-            return null;
-        } else {
-            return null;
-        }
     }
 }
