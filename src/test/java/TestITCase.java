e9691df223b1031544b14de67d9c359184ea2b0e
pawelmol <pawel@mol.com.pl>
2015-08-06 15:15:50 +0000

    RZ#2564 Serwer z39.50
    git-svn-id: https://svn.mol.com.pl/svn/mol.net/src/trunk/java/z3950-server@8475 74f6632f-f9af-439b-9eda-9f9433cb0ea0

--- src/test/java/TestITCase.java
+++ src/test/java/TestITCase.java
@@ -1,28 +1,22 @@
 
-import java.util.regex.Pattern;
+import java.io.IOException;
 import org.junit.Test;
+import org.xml.sax.SAXException;
 
 /**
  *
  * @author Paweł
  */
 public class TestITCase {
-    /*
-     Algorytm dodawania znaków umownych:
-     dla każdego pola
-     dla każdego podpola, sprawdzaj czy występuje za nim podpole, gdy tak to wstaw na końcu pola to co jest specyfikowane w podpolu
-     */
-    
-    
 
-    @Test
-    public void test() {
-        String[] isbns = new String[6];
-        isbns[0] = "83-01-01373-1";
-        isbns[1] = "978-83-01-01373-7";
-        isbns[2] = "978-83-01-01373-X";
-        isbns[3] = "83-01-01373-7 coś";
-        isbns[4] = "coś 83-01-01373-7 coś";
-        isbns[5] = "coś 83-01-01373-1 coś  83-01-01373-2";
-    }
+	@Test
+	public void test() throws IOException, SAXException {
+		String[] isbns = new String[6];
+		isbns[0] = "83-01-01373-1";
+		isbns[1] = "978-83-01-01373-7";
+		isbns[2] = "978-83-01-01373-X";
+		isbns[3] = "83-01-01373-7 coś";
+		isbns[4] = "coś 83-01-01373-7 coś";
+		isbns[5] = "coś 83-01-01373-1 coś  83-01-01373-2";
+	}
 }
