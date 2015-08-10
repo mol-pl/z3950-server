dc041cbdacc7559982b06290af1ee7ed59a9b09c
pawelmol <pawel@mol.com.pl>
2015-08-10 14:31:31 +0000

    RZ#2599 Uzupełnienie ustawień baz bibliograficznych tenanta w SuperMOL (REQ1341)
    git-svn-id: https://svn.mol.com.pl/svn/mol.net/src/trunk/java/z3950-server@8498 74f6632f-f9af-439b-9eda-9f9433cb0ea0

--- src/main/java/pl/mol/molnet/z3950/server/HttpDataProvider.java
+++ src/main/java/pl/mol/molnet/z3950/server/HttpDataProvider.java
@@ -24,185 +24,180 @@ import org.marc4j.marc.Record;
 /**
  * Dostarcza metody do komunikacji z kontrolerem w molnet
  *
- * @author Pawe�
+ * @author Paweł
  */
 public class HttpDataProvider {
 
-    private final Pattern patternTitle = Pattern.compile("bib-1\\.1\\.4");
-    private final Pattern patternAuthor = Pattern.compile("bib-1\\.1\\.1003");
-    private final Pattern patternIsbn = Pattern.compile("bib-1\\.1\\.7");
-    private final Pattern patternPublDate = Pattern.compile("bib-1\\.1\\.31");
+	private final Pattern patternTitle = Pattern.compile("bib-1\\.1\\.4");
+	private final Pattern patternAuthor = Pattern.compile("bib-1\\.1\\.1003");
+	private final Pattern patternIsbn = Pattern.compile("bib-1\\.1\\.7");
+	private final Pattern patternPublDate = Pattern.compile("bib-1\\.1\\.31");
 
-    private String attrset;
+	private String attrset;
 
-    private String prepareGetParams(HttpQueryParams params, Integer start, Integer limit) throws UnsupportedEncodingException {
-        String urlParameters = null;
+	private String prepareGetParams(HttpQueryParams params, Integer start, Integer limit) throws UnsupportedEncodingException {
+		String urlParameters = null;
 
-        if (params.getAuthor() != null) {
-            urlParameters = "author=" + URLEncoder.encode(params.getAuthor(), "UTF-8");
-        }
+		if (params.getAuthor() != null) {
+			urlParameters = "author=" + URLEncoder.encode(params.getAuthor(), "UTF-8");
+		}
 
-        if (params.getTitle() != null) {
-            if (urlParameters != null) {
-                urlParameters = urlParameters + "&title=" + URLEncoder.encode(params.getTitle(), "UTF-8");
-            } else {
-                urlParameters = "title=" + URLEncoder.encode(params.getTitle(), "UTF-8");
-            }
-        }
+		if (params.getTitle() != null) {
+			if (urlParameters != null) {
+				urlParameters = urlParameters + "&title=" + URLEncoder.encode(params.getTitle(), "UTF-8");
+			} else {
+				urlParameters = "title=" + URLEncoder.encode(params.getTitle(), "UTF-8");
+			}
+		}
 
-        if (params.getIsbn() != null) {
-            if (urlParameters != null) {
-                urlParameters = urlParameters + "&isbn=" + URLEncoder.encode(params.getIsbn(), "UTF-8");
-            } else {
-                urlParameters = "isbn=" + URLEncoder.encode(params.getIsbn(), "UTF-8");
-            }
-        }
+		if (params.getIsbn() != null) {
+			if (urlParameters != null) {
+				urlParameters = urlParameters + "&isbn=" + URLEncoder.encode(params.getIsbn(), "UTF-8");
+			} else {
+				urlParameters = "isbn=" + URLEncoder.encode(params.getIsbn(), "UTF-8");
+			}
+		}
 
-        if (params.getPublDate() != null) {
-            if (urlParameters != null) {
-                urlParameters = urlParameters + "&date=" + URLEncoder.encode(params.getPublDate(), "UTF-8");
-            } else {
-                urlParameters = "date=" + URLEncoder.encode(params.getPublDate(), "UTF-8");
-            }
-        }
+		if (params.getPublDate() != null) {
+			if (urlParameters != null) {
+				urlParameters = urlParameters + "&date=" + URLEncoder.encode(params.getPublDate(), "UTF-8");
+			} else {
+				urlParameters = "date=" + URLEncoder.encode(params.getPublDate(), "UTF-8");
+			}
+		}
 
-        if (params.getLibraryId() != null) {
-            if (urlParameters != null) {
-                urlParameters = urlParameters + "&library=" + URLEncoder.encode(params.getLibraryId().toString(), "UTF-8");
-            } else {
-                urlParameters = "library=" + URLEncoder.encode(params.getLibraryId().toString(), "UTF-8");
-            }
-        }
+		if (params.getBibliographicDbAlias() != null) {
+			if (urlParameters != null) {
+				urlParameters = urlParameters + "&bibdbalias=" + URLEncoder.encode(params.getBibliographicDbAlias(), "UTF-8");
+			} else {
+				urlParameters = "bibdbalias=" + URLEncoder.encode(params.getBibliographicDbAlias(), "UTF-8");
+			}
+		}
 
-        if (start != null) {
-            if (urlParameters != null) {
-                urlParameters = urlParameters + "&start=" + URLEncoder.encode(start.toString(), "UTF-8");
-            } else {
-                urlParameters = "start=" + URLEncoder.encode(start.toString(), "UTF-8");
-            }
-        }
+		if (start != null) {
+			if (urlParameters != null) {
+				urlParameters = urlParameters + "&start=" + URLEncoder.encode(start.toString(), "UTF-8");
+			} else {
+				urlParameters = "start=" + URLEncoder.encode(start.toString(), "UTF-8");
+			}
+		}
 
-        if (limit != null) {
-            if (urlParameters != null) {
-                urlParameters = urlParameters + "&limit=" + URLEncoder.encode(limit.toString(), "UTF-8");
-            } else {
-                urlParameters = "limit=" + URLEncoder.encode(limit.toString(), "UTF-8");
-            }
-        }
+		if (limit != null) {
+			if (urlParameters != null) {
+				urlParameters = urlParameters + "&limit=" + URLEncoder.encode(limit.toString(), "UTF-8");
+			} else {
+				urlParameters = "limit=" + URLEncoder.encode(limit.toString(), "UTF-8");
+			}
+		}
 
-        return urlParameters;
-    }
+		return urlParameters;
+	}
 
-    public int getCount(HttpQueryParams params) throws UnsupportedEncodingException, MalformedURLException, IOException, Exception {
-        String url = params.getTenantUrl() + "/api/z3950server/count?" + prepareGetParams(params, null, null);
+	public int getCount(HttpQueryParams params) throws UnsupportedEncodingException, MalformedURLException, IOException, Exception {
+		String url = params.getTenantUrl() + "/api/z3950server/count?" + prepareGetParams(params, null, null);
 
-        HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
+		HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
 
-        con.setRequestMethod("GET");
+		con.setRequestMethod("GET");
 
-        if (con.getResponseCode() == 200) {
-            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            String inputLine = in.readLine();
-            in.close();
+		if (con.getResponseCode() == 200) {
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String inputLine = in.readLine();
+			in.close();
 
-            try {
-                return Integer.parseInt(inputLine);
-            } catch (NumberFormatException ex) {
-                throw new Exception("Database not found");
-            }
-        } else {
-            throw new Exception("Database not found");
-        }
-    }
+			try {
+				return Integer.parseInt(inputLine);
+			} catch (NumberFormatException ex) {
+				throw new Exception("Database not found");
+			}
+		} else {
+			throw new Exception("Database not found");
+		}
+	}
 
-    public Record[] getList(HttpQueryParams params, Integer start, Integer limit) throws UnsupportedEncodingException, MalformedURLException, IOException, Exception {
-        String url = params.getTenantUrl() + "/api/z3950server/?" + prepareGetParams(params, start, limit);
+	public Record[] getList(HttpQueryParams params, Integer start, Integer limit) throws UnsupportedEncodingException, MalformedURLException, IOException, Exception {
+		String url = params.getTenantUrl() + "/api/z3950server/?" + prepareGetParams(params, start, limit);
 
-        HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
+		HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
 
-        con.setRequestMethod("GET");
+		con.setRequestMethod("GET");
 
-        if (con.getResponseCode() == 200) {
-            List<Record> list = new ArrayList<>();
-            MarcReader reader = new MarcStreamReader(con.getInputStream(), "UTF8");
-            while (reader.hasNext()) {
-                list.add(reader.next());
-            }
+		if (con.getResponseCode() == 200) {
+			List<Record> list = new ArrayList<>();
+			MarcReader reader = new MarcStreamReader(con.getInputStream(), "UTF8");
+			while (reader.hasNext()) {
+				list.add(reader.next());
+			}
 
-            Record[] records = new Record[limit];
-            return (Record[]) list.toArray(records);
-        } else {
-            throw new Exception("Database not found");
-        }
-    }
+			Record[] records = new Record[limit];
+			return (Record[]) list.toArray(records);
+		} else {
+			throw new Exception("Database not found");
+		}
+	}
 
-    /**
-     * Wydziela z nazwy kolekcji nazw� tenanta i id bazy
-     *
-     * @param molnetUrlDomain
-     * @param collection
-     * @param params
-     * @throws java.lang.Exception
-     */
-    public void parseDbName(String molnetUrlDomain, String collection, HttpQueryParams params) throws Exception {
-        if (molnetUrlDomain == null || collection == null) {
-            throw new Exception("Cannot create tenant url");
-        }
+	/**
+	 * Wydziela z nazwy kolekcji nazw� tenanta i id bazy
+	 *
+	 * @param molnetUrlDomain
+	 * @param collection
+	 * @param params
+	 * @throws java.lang.Exception
+	 */
+	public void parseDbName(String molnetUrlDomain, String collection, HttpQueryParams params) throws Exception {
+		if (molnetUrlDomain == null || collection == null) {
+			throw new Exception("Cannot create tenant url");
+		}
 
-        String tenantName = collection;
-        int index = collection.indexOf("/");
-        if (index != -1) {
-            try {
-                params.setLibraryId(Long.parseLong(collection.substring(index + 1)));
-            } catch (NumberFormatException ex) {
-                throw new Exception("Invalid library identifier");
-            }
+		String tenantName = collection;
+		int index = collection.indexOf("/");
+		if (index != -1) {
+			params.setBibliographicDbAlias(collection.substring(index + 1));
+			tenantName = tenantName.substring(0, index);
+		}
 
-            tenantName = tenantName.substring(0, index);
-        }
+		//tworzenie urla tenenta
+		params.setTenantUrl(String.format("http://%s.%s", tenantName, molnetUrlDomain));
+	}
 
-        //tworzenie urla tenenta
-        params.setTenantUrl(String.format("http://%s.%s", tenantName, molnetUrlDomain));
-    }
+	//TODO doda� sprawdzanie and or
+	public void parseQuery(QueryNode qn, HttpQueryParams params) throws Exception {
 
-    //TODO doda� sprawdzanie and or
-    public void parseQuery(QueryNode qn, HttpQueryParams params) throws Exception {
+		if (qn instanceof InternalModelRootNode) {
+			InternalModelRootNode imrn = (InternalModelRootNode) qn;
+			parseQuery(imrn.getChild(), params);
+		} else if (qn instanceof InternalModelNamespaceNode) {
+			InternalModelNamespaceNode imns = (InternalModelNamespaceNode) qn;
+			attrset = imns.getAttrset();
+			parseQuery(imns.getChild(), params);
+		} else if (qn instanceof ComplexNode) {
+			ComplexNode cn = (ComplexNode) qn;
+			parseQuery(cn.getLHS(), params);
+			parseQuery(cn.getRHS(), params);
+		} else if (qn instanceof AttrPlusTermNode) {
+			AttrPlusTermNode aptn = (AttrPlusTermNode) qn;
+			Object a = aptn.getAttr("AccessPoint");
 
-        if (qn instanceof InternalModelRootNode) {
-            InternalModelRootNode imrn = (InternalModelRootNode) qn;
-            parseQuery(imrn.getChild(), params);
-        } else if (qn instanceof InternalModelNamespaceNode) {
-            InternalModelNamespaceNode imns = (InternalModelNamespaceNode) qn;
-            attrset = imns.getAttrset();
-            parseQuery(imns.getChild(), params);
-        } else if (qn instanceof ComplexNode) {
-            ComplexNode cn = (ComplexNode) qn;
-            parseQuery(cn.getLHS(), params);
-            parseQuery(cn.getRHS(), params);
-        } else if (qn instanceof AttrPlusTermNode) {
-            AttrPlusTermNode aptn = (AttrPlusTermNode) qn;
-            Object a = aptn.getAttr("AccessPoint");
+			if (a != null) {
+				if (a instanceof AttrValue) {
+					AttrValue av = (AttrValue) a;
+					String val = av.getWithDefaultNamespace(attrset);
 
-            if (a != null) {
-                if (a instanceof AttrValue) {
-                    AttrValue av = (AttrValue) a;
-                    String val = av.getWithDefaultNamespace(attrset);
-
-                    if (patternTitle.matcher(val).matches()) {
-                        params.setTitle((String) aptn.getTerm());
-                    } else if (patternAuthor.matcher(val).matches()) {
-                        params.setAuthor((String) aptn.getTerm());
-                    } else if (patternIsbn.matcher(val).matches()) {
-                        params.setIsbn((String) aptn.getTerm());
-                    } else if (patternPublDate.matcher(val).matches()) {
-                        params.setPublDate((String) aptn.getTerm());
-                    }
-                } else {
-                    throw new Exception("Cannot parse query");
-                }
-            } else {
-                throw new Exception("Cannot parse query");
-            }
-        }
-    }
+					if (patternTitle.matcher(val).matches()) {
+						params.setTitle((String) aptn.getTerm());
+					} else if (patternAuthor.matcher(val).matches()) {
+						params.setAuthor((String) aptn.getTerm());
+					} else if (patternIsbn.matcher(val).matches()) {
+						params.setIsbn((String) aptn.getTerm());
+					} else if (patternPublDate.matcher(val).matches()) {
+						params.setPublDate((String) aptn.getTerm());
+					}
+				} else {
+					throw new Exception("Cannot parse query");
+				}
+			} else {
+				throw new Exception("Cannot parse query");
+			}
+		}
+	}
 }
--- src/main/java/pl/mol/molnet/z3950/server/HttpQueryParams.java
+++ src/main/java/pl/mol/molnet/z3950/server/HttpQueryParams.java
@@ -3,100 +3,98 @@ package pl.mol.molnet.z3950.server;
 /**
  * Parametry zapytania do Molnet.
  *
- * @author Pawe�
+ * @author Paweł
  */
 public class HttpQueryParams {
 
-    private String title;
-    private String author;
-    private String isbn;
-    private String publDate;
-    private Long libraryId;
-    private String tenantUrl;
+	private String title;
+	private String author;
+	private String isbn;
+	private String publDate;
+	private String bibliographicDbAlias;
+	private String tenantUrl;
 
-    /**
-     * @return the title
-     */
-    public String getTitle() {
-        return title;
-    }
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
 
-    /**
-     * @param title the title to set
-     */
-    public void setTitle(String title) {
-        this.title = title;
-    }
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-    /**
-     * @return the author
-     */
-    public String getAuthor() {
-        return author;
-    }
+	/**
+	 * @return the author
+	 */
+	public String getAuthor() {
+		return author;
+	}
 
-    /**
-     * @param author the author to set
-     */
-    public void setAuthor(String author) {
-        this.author = author;
-    }
+	/**
+	 * @param author the author to set
+	 */
+	public void setAuthor(String author) {
+		this.author = author;
+	}
 
-    /**
-     * @return the isbn
-     */
-    public String getIsbn() {
-        return isbn;
-    }
+	/**
+	 * @return the isbn
+	 */
+	public String getIsbn() {
+		return isbn;
+	}
 
-    /**
-     * @param isbn the isbn to set
-     */
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
+	/**
+	 * @param isbn the isbn to set
+	 */
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
 
-    /**
-     * @return the publDate
-     */
-    public String getPublDate() {
-        return publDate;
-    }
+	/**
+	 * @return the publDate
+	 */
+	public String getPublDate() {
+		return publDate;
+	}
 
-    /**
-     * @param publDate the publDate to set
-     */
-    public void setPublDate(String publDate) {
-        this.publDate = publDate;
-    }
+	/**
+	 * @param publDate the publDate to set
+	 */
+	public void setPublDate(String publDate) {
+		this.publDate = publDate;
+	}
 
-    /**
-     * @return the libraryId
-     */
-    public Long getLibraryId() {
-        return libraryId;
-    }
+	/**
+	 * @return the bibliographicDbAlias
+	 */
+	public String getBibliographicDbAlias() {
+		return bibliographicDbAlias;
+	}
 
-    /**
-     * @param libraryId the libraryId to set
-     */
-    public void setLibraryId(Long libraryId) {
-        this.libraryId = libraryId;
-    }
+	/**
+	 * @param bibliographicDbAlias the bibliographicDbAlias to set
+	 */
+	public void setBibliographicDbAlias(String bibliographicDbAlias) {
+		this.bibliographicDbAlias = bibliographicDbAlias;
+	}
 
-    /**
-     * @return the tenantUrl
-     */
-    public String getTenantUrl() {
-        return tenantUrl;
-    }
+	/**
+	 * @return the tenantUrl
+	 */
+	public String getTenantUrl() {
+		return tenantUrl;
+	}
 
-    /**
-     * @param tenantUrl the tenantUrl to set
-     */
-    public void setTenantUrl(String tenantUrl) {
-        this.tenantUrl = tenantUrl;
-    }
-    
-    
+	/**
+	 * @param tenantUrl the tenantUrl to set
+	 */
+	public void setTenantUrl(String tenantUrl) {
+		this.tenantUrl = tenantUrl;
+	}
 }
