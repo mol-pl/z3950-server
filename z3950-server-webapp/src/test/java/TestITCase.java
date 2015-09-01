96743258ab952e179464dead649d733ea6aeecd7
pawelmol <pawel@mol.com.pl>
2015-09-01 14:44:44 +0000

    RZ#2659 Problem z połączeniem Z39.50 na produkcji (blocker)
    git-svn-id: https://svn.mol.com.pl/svn/mol.net/src/trunk/java/z3950-server@8690 74f6632f-f9af-439b-9eda-9f9433cb0ea0

--- z3950-server-webapp/src/test/java/TestITCase.java
+++ z3950-server-webapp/src/test/java/TestITCase.java
@@ -1,13 +1,32 @@
 
+import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
+import org.marc4j.marc.Record;
+import pl.mol.molnet.z3950.server.HttpDataProvider;
+import pl.mol.molnet.z3950.server.HttpQueryParams;
 
 /**
  *
  * @author Paweł
  */
+//@Ignore
 public class TestITCase {
-  
-    @Test
-    public void test() {
-    }
+
+	private String url = "http://test.localhost:8080/api/z3950server/?author=a&start=0&limit=100";
+	//private String url = "https://mol-prod--beta.molnet.mol.pl/api/z3950server/?author=arctowa&start=0&limit=1";
+	//private String url = "http://dev.mol.com.pl/api/z3950server/?author=a&start=0&limit=10";
+
+	@Test
+	public void test() throws Exception {
+
+		HttpDataProvider httpDataProvider = new HttpDataProvider();
+		HttpQueryParams params = new HttpQueryParams();
+		httpDataProvider.parseDbName("localhost:8080", "test", params);
+		params.setAuthor("a");
+
+		int count = httpDataProvider.getCount(params);
+
+		Record[] list = httpDataProvider.getList(params, 0, count);
+		assertNotNull(list);
+	}
 }
